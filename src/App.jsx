5b1705c016407bed6bea418a0fdfeec40f9fc32d
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2023-08-21 16:43:11 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  const mark1= "Aliyu"
+  const checker= false
+  return ( <>
+  <h1 className="conn"> Helloo {checker ? '' : mark1} </h1>
+  <div>
+    <p>
+      testing testing
+    </p>
+  </div>
+ <p>
+  house
+ </p>
+ </>
+
+  )
+}
+
+export default App
